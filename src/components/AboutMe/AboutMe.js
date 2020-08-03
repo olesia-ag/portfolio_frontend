b420f92d9f4e69d365650f28b6feaf5be87d5798
Olesia Guidi <olgarbuzova@gmail.com>
2020-08-03 12:55:17 -0400

    chnged about me

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -5,7 +5,7 @@ function App() {
 	return (
 		<div className={classes.AboutMe}>
 			<p>
-			 I've discovered my love for technology at the age of 14 when I got my first computer, taught myself basic HTML and created my first fan webpage (it was dedicated to Olsen twins, if you're curious). <br className={classes.brMajor}/> 15 years, one immigration, one degree in art history and a career in hospitality later I decided to take a leap of faith and go back to what made me happy long time ago - that is, sitting in front of the monitor and thinking 'how to make this work'<br className={classes.brMajor}/>
+			 I've discovered my love for technology at the age of 14 when I got my first computer and taught myself basic HTML/CSS to create an Olsen twins fan webpage (I know, 2000s were wild). <br className={classes.brMajor}/> 15 years, one immigration, one degree in art history and a career in hospitality later I decided to take a leap of faith and go back to what made me happy long time ago - that is, sitting in front of the monitor and thinking 'how to make this work'<br className={classes.brMajor}/>
 			 So, say hi and let me know what was your favorite Olsen twins movie:
 
 
