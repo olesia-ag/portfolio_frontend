df3e8e628af78193877a3d98a04c049d7de0c44f
Olesia Guidi <olgarbuzova@gmail.com>
2020-08-17 14:52:18 -0400

    css prefixes

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,17 +1,29 @@
-import React from 'react'
-import classes from './AboutMe.module.css'
+import React, { useState } from 'react';
+import classes from './AboutMe.module.css';
 
 function App() {
+	const [show, setShow] = useState(false);
+
+	const handleShowClick = () => {
+		setShow(true);
+	};
+
 	return (
 		<div className={classes.AboutMe}>
 			<p>
-			 I've discovered my love for technology at the age of 14 when I got my first computer and taught myself basic HTML/CSS to create an Olsen twins fan webpage (I know, 2000s were wild). <br className={classes.brMajor}/> 15 years, one immigration, one degree in art history and a career in hospitality later I decided to take a leap of faith and go back to what made me happy long time ago - that is, sitting in front of the monitor and thinking 'how to make this work'<br className={classes.brMajor}/>
-			 So, say hi and let me know what was your favorite Olsen twins movie:
-
-
+				I've discovered my love for technology at the age of 14 when I got my
+				first computer and taught myself basic HTML/CSS to create an Olsen twins
+				fan webpage (I know, 2000s were wild).{' '}
+				<br className={classes.brMajor} /> 15 years, one immigration, one degree
+				in art history and a career in hospitality later I decided to take a
+				leap of faith and go back to what made me happy long time ago - that is,
+				sitting in front of the monitor and thinking 'how to make this work'
+				<br className={classes.brMajor} />
+				React.js remains my favorite library and I keep digging deeper into its internals and principles of functional programming in JS.
+				So, drop me a line and let me know what was your favorite Olsen twins movie:
 			</p>
 		</div>
-	)
+	);
 }
 
-export default App
+export default App;
