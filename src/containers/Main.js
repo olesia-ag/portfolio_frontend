df915ce1cb067436563912969409e0ebcca6d2fe
Olesia Guidi <olgarbuzova@gmail.com>
2021-03-10 15:31:39 -0500

    added vectors

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,21 +4,36 @@ import ContactMe from './ContactMe/ContactMe';
 import AboutMe from '../components/AboutMe/AboutMe';
 import Skills from '../components/Skills/Skills';
 import Connect from '../components/Connect/Connect';
-
-
+import SmallEllipse from '../assets/vectors/small_ellipse.png';
+import LargeEllipse from '../assets/vectors/large_ellipse.png';
+import Vector from '../assets/vectors/vector.png';
 
 function Main() {
 	return (
 		<div className={classes.Main}>
 			<header className={classes.MainHeader}>
-				<div className={classes.Portrait}>hi</div>
-				{/* <div className={classes.AboutMeContainer}>
-					<div className={classes.first}>Hello World, I am Olesia</div>
-					<div className={classes.second}>a fullstack developer</div>
-					<div className={classes.third}>living in NYC</div>
-				</div> */}
+				<div className={classes.Overlay}> </div>
 			</header>
-			{/* id is used for scrolling to an item, do not remove */}
+			<div className={classes.ImageContainer}>
+				<img
+					src={SmallEllipse}
+					style={{ position: 'absolute', top: '-5.5rem', left: '-6rem' }}
+					id='small-ellipse'
+					alt=''
+				/>
+				<img src={Vector} style={{ position: 'absolute', left: "-.4rem", top: "-.4rem" }} alt='' />
+				<img src={LargeEllipse} style={{ position: 'absolute', top: "45rem", left: "8.5rem" }} alt='' />
+			</div>
+			<div className={classes.AboutMeContainer}>
+				<p>Hello World,</p>
+				<h1>I am Olesia.</h1>
+				<p>
+					a fullstack developer
+					<br />
+					living in NYC
+				</p>
+			</div>
+
 			<div className={classes.AboutMe} id='about-me'>
 				<AboutMe />
 				<Skills />
